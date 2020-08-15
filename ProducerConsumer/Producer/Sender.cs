05cfbbd01a26ea60b94ec7316ddb845e516a1c76
shockliang <shockwater@gmail.com>
2020-08-15 17:49:20 +0800

    Adding Producer project.

---
+++ ProducerConsumer/Producer/Sender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using(var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                var message = "Starting with .net core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+                
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine($"Send messages {message}");
+            }
+
+            Console.WriteLine("Press any key to exit sender app");
+            Console.ReadLine();
+        }
+    }
+}
