d69d2df7ad3583559a1db3274e6cd0dcd0ec4e48
shockliang <shockwater@gmail.com>
2020-08-22 14:00:56 +0800

    Implementing banking controller.

---
+++ MicroRabbit/Microservices/Banking/Api/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly ILogger<BankingController> _logger;
+        private readonly IAccountService _accountService;
+
+        public BankingController(ILogger<BankingController> logger, IAccountService accountService)
+        {
+            _logger = logger;
+            _accountService = accountService;
+        }
+
+        // api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
