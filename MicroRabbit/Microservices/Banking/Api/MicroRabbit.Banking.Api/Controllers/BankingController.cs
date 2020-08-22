b71b65ccdd41960528c75423c6c89b63238511fc
shockliang <shockwater@gmail.com>
2020-08-23 00:14:46 +0800

    Setup ioc configurations.

--- MicroRabbit/Microservices/Banking/Api/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/Microservices/Banking/Api/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -33,6 +33,7 @@ namespace MicroRabbit.Banking.Api.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] AccountTransfer accountTransfer)
         {
+            _accountService.Transfer(accountTransfer);
             return Ok(accountTransfer);
         }
     }
