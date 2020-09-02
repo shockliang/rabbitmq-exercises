5152924fd6fb47b7f8b121934d470e3eda54245c
shockliang <shockwater@gmail.com>
2020-09-02 21:22:56 +0800

    Adding proxy transfer service.

---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using MicroRabbit.MVC.Models.DTO;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        public Task Transfer(TransferDto transferDto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
