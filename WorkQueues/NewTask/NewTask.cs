9857ce0caa35c71143b74eaed290d0028ec1e8ea
shockliang <shockwater@gmail.com>
2020-09-26 00:12:38 +0800

    Adding new task creation.

--- WorkQueues/NewTask/NewTask.cs
+++ WorkQueues/NewTask/NewTask.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace NewTask
 {
@@ -6,7 +8,22 @@ namespace NewTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "task_queue",
+                basicProperties: properties,
+                body: body);
         }
+
+        private static string GetMessage(string[] args) 
+            => args.Length > 0 ? string.Join(" ", args) : "Hello World!";
     }
 }
