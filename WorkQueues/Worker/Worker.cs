fe35e63932517803d2518aaca0c8fe424114bd3a
shockliang <shockwater@gmail.com>
2020-09-29 00:05:19 +0800

    Adding worker.

--- WorkQueues/Worker/Worker.cs
+++ WorkQueues/Worker/Worker.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Worker
 {
@@ -6,7 +10,31 @@ namespace Worker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(
+                queue: "task_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+                var dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+            };
+            channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
