abead59338f652e7a98601838cc536961878499c
shockliang <shockwater@gmail.com>
2021-03-18 21:37:39 +0800

    Adding publish and subscribe exercise.

---
+++ PublishAndSubscribe/EmitLog/EmitLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace EmitLog
+{
+    class EmitLog
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            for (var i = 0; i < 100; i++)
+            {
+                var message = GetMessage(args, i);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs",
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+           
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args, int idx)
+        {
+            return ((args.Length > 0)
+                ? string.Join(" ", args)
+                : $"info: Hello World! {idx}");
+        }
+    }
+}
